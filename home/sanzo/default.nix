ead4ca37255344fac8b34ba52bf55eaaa5c771cf
Andrea Ghensi <andrea.ghensi@gmail.com>
2024-10-19 22:53:33 +0200

    feat(sanzo): helix instead of zed-editor, add nushell, move ssh config to its own file

--- home/sanzo/default.nix
+++ home/sanzo/default.nix
@@ -1,55 +1,49 @@
 { pkgs, ... }:
-let
-  identityFile = "/home/sanzo/.ssh/id_rsa";
-in
 {
+  imports = [
+    ./ssh.nix
+  ];
+
   home = {
     username = "sanzo";
     # homeDirectory = "/home/sanzo";
     stateVersion = "24.05";
   };
+  programs.home-manager.enable = true;
+
   home.packages = with pkgs; [
-    zed-editor
     devbox
-    direnv
-    lazygit
     anytype
     blender
     qbittorrent
   ];
   programs.vscode.enable = true;
-  programs.home-manager.enable = true;
+  programs.direnv.enable = true;
   programs.git = {
     enable = true;
     userName = "Andrea Ghensi";
     userEmail = "andrea.ghensi@gmail.com";
     extraConfig.push.autoSetupRemote = true;
   };
-  programs.ssh = {
+  programs.lazygit.enable = true;
+  programs.helix = {
     enable = true;
-    matchBlocks = {
-      holodeck = {
-        hostname = "192.168.1.220";
-        inherit identityFile;
-      };
-      viewscreen = {
-        hostname = "192.168.1.224";
-        inherit identityFile;
-      };
-      zora = {
-        hostname = "192.168.1.225";
-        inherit identityFile;
-      };
-      "gitlab.com" = {
-        inherit identityFile;
-      };
-      sveglia = {
-        hostname = "192.168.1.209";
-        inherit identityFile;
-        port = 8022;
-      };
+    defaultEditor = true;
+    settings = {
+      editor.line-number = "relative";
     };
+    # languages = {};
+    # themes = {};
+    # ignores = [];
   };
+  # TODO: https://mynixos.com/home-manager/options/programs.nushell
+  programs.nushell.enable = true;
+  programs.zellij = {
+    enable = true;
+    enableBashIntegration = true;
+    # settings = {};
+  };
+
   services.keybase.enable = true;
   services.kbfs.enable = true;
 
@@ -57,8 +51,4 @@ in
     enable = true;
   };
 
-  programs.zellij = {
-    enable = true;
-    enableBashIntegration = true;
-  };
 }
