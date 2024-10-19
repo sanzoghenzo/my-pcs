2cab5ba9dc6d1d5885a55bbb9ba4b53e426652a7
Andrea Ghensi <andrea.ghensi@gmail.com>
2024-10-19 23:13:22 +0200

    feat(zora): enable podman

--- systems/zora/default.nix
+++ systems/zora/default.nix
@@ -9,6 +9,7 @@
     ./hardware-configuration.nix
     ../common/base
     ../common/services/tailscale.nix
+    ../common/services/podman.nix
     ../common/root-ssh.nix
     ../common/vm.nix
     ../common/eth0-wol.nix
