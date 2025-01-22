f7303a4aa162ddd019ba70d10146d61b4f8378d4
Andrea Ghensi <andrea.ghensi@gmail.com>
2025-01-22 19:39:21 +0100

    stash zora services

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -8,4 +8,12 @@ rec {
   horus = kodi-py311.packages.callPackage ./horus { };
   protobuf-kodi = kodi-py311.packages.callPackage ./protobuf { };
   hyperion-kodi = kodi-py311.packages.callPackage ./hyperion-kodi { };
+
+  silverbullet = pkgs.silverbullet.overrideAttrs (attrs: {
+    version = "0.10.1";
+    src = pkgs.fetchurl {
+      url = "https://github.com/silverbulletmd/silverbullet/releases/download/0.10.1/silverbullet.js";
+      hash = "sha256-4ZnA5cmLDlEUpeTBgz6Wg3XK3JJpgdt9bf9Eg7o82T8=";
+    };
+  });
 }
