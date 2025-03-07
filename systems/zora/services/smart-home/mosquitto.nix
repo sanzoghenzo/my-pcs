d71a8ddf2b4a79a3d8a9920ef61c92ae3f1373f6
sanzoghenzo <andrea.ghensi@gmail.com>
2025-03-08 00:34:06 +0100

    feat(zora): remove mqtt server for meross
    Meros power strip is gone, no more need for stupid configuration

--- systems/zora/services/smart-home/mosquitto.nix
+++ systems/zora/services/smart-home/mosquitto.nix
@@ -1,25 +1,26 @@
 {config, ...}:
-let
-  domain = "mqtt.sanzoghenzo.com";
-  certDir = config.security.acme.certs."${domain}".directory;
-in
+# let
+#   domain = "mqtt.sanzoghenzo.com";
+#   certDir = config.security.acme.certs."${domain}".directory;
+# in
 {
   age.secrets.mosquitto-root-password.file = ../../../../secrets/mosquitto-root-password.age;
 
+  # TSL needed only for meross power strip, ignoring
   # this is to acess the cloudflare secret
-  users.groups.traefik.members = [ "acme" ];
-  security.acme = {
-    acceptTerms = true;
-    defaults = {
-      email = "andrea.ghensi@gmail.com";
-      dnsProvider = "cloudflare";
-      dnsResolver = "1.1.1.1:53";
-      credentialFiles = {
-        CF_DNS_API_TOKEN_FILE = config.age.secrets.cloudflare-dns-api-token.path;
-      };
-    };
-    certs."${domain}".keyType = "rsa2048";
-  };
+  # users.groups.traefik.members = [ "acme" ];
+  # security.acme = {
+  #   acceptTerms = true;
+  #   defaults = {
+  #     email = "andrea.ghensi@gmail.com";
+  #     dnsProvider = "cloudflare";
+  #     dnsResolver = "1.1.1.1:53";
+  #     credentialFiles = {
+  #       CF_DNS_API_TOKEN_FILE = config.age.secrets.cloudflare-dns-api-token.path;
+  #     };
+  #   };
+  #   certs."${domain}".keyType = "rsa2048";
+  # };
 
   users.groups.acme.members = ["mosquitto"];
   services.mosquitto = {
@@ -32,32 +33,39 @@ in
           passwordFile = config.age.secrets.mosquitto-root-password.path;
         };
       }
-      {
-        # for meross strip
-        acl = [
-          "topic readwrite /appliance/#"
-          "topic readwrite /app/#"
-        ];
-        port = 8883;
-        omitPasswordAuth = true;
-        settings = {
-          allow_anonymous = true;
-          require_certificate = false;
-          keyfile = "${certDir}/key.pem";
-          certfile = "${certDir}/cert.pem";
-          cafile = "${certDir}/chain.pem";
-          tls_version = "tlsv1.1";
-          ciphers = "ALL:@SECLEVEL=0";
-        };
-      }
+      # {
+      #   # for meross strip
+      #   acl = [
+      #     "topic readwrite /appliance/#"
+      #     "topic readwrite /app/#"
+      #   ];
+      #   port = 8883;
+      #   omitPasswordAuth = true;
+      #   settings = {
+      #     allow_anonymous = true;
+      #     require_certificate = false;
+      #     keyfile = "${certDir}/key.pem";
+      #     certfile = "${certDir}/cert.pem";
+      #     cafile = "${certDir}/chain.pem";
+      #     # TODO: test like this
+      #     # tls_version = "tlsv1.1";
+      #     # ciphers = "ALL:@SECLEVEL=0";
+      #     use_username_as_clientid = true;
+      #   };
+      # }
     ];
   };
   networking.firewall = {
     allowedTCPPorts = [
       1883
-      8883
+      # 8883
     ];
   };
 
-  # TODO: add dns entry (but no reverse proxy)
+  services.adguardhome.settings.filtering.rewrites = [
+    {
+      domain = "mqtt.sanzoghenzo.com";
+      answer = "192.168.1.225";
+    }
+  ];
 }
