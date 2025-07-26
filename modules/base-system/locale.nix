d2ed53b70f32e62b243874ef35cd4cf8fa02ad02
sanzoghenzo <andrea.ghensi@gmail.com>
2025-07-26 09:47:54 +0200

    feat: update flakes

--- modules/base-system/locale.nix
+++ modules/base-system/locale.nix
@@ -8,17 +8,17 @@ _: {
   };
 
   i18n = {
-    defaultLocale = "it_IT.utf8";
+    defaultLocale = "it_IT.UTF-8";
     extraLocaleSettings = {
-      LC_ADDRESS = "it_IT.utf8";
-      LC_IDENTIFICATION = "it_IT.utf8";
-      LC_MEASUREMENT = "it_IT.utf8";
-      LC_MONETARY = "it_IT.utf8";
-      LC_NAME = "it_IT.utf8";
-      LC_NUMERIC = "it_IT.utf8";
-      LC_PAPER = "it_IT.utf8";
-      LC_TELEPHONE = "it_IT.utf8";
-      LC_TIME = "it_IT.utf8";
+      LC_ADDRESS = "it_IT.UTF-8";
+      LC_IDENTIFICATION = "it_IT.UTF-8";
+      LC_MEASUREMENT = "it_IT.UTF-8";
+      LC_MONETARY = "it_IT.UTF-8";
+      LC_NAME = "it_IT.UTF-8";
+      LC_NUMERIC = "it_IT.UTF-8";
+      LC_PAPER = "it_IT.UTF-8";
+      LC_TELEPHONE = "it_IT.UTF-8";
+      LC_TIME = "it_IT.UTF-8";
     };
   };
 }
